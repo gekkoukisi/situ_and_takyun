99c2e739424337dfb270c2e5e90b1e8da7a02d89
valai <goriakku@gmail.com>
2015-09-22 23:18:23 +0900

    プロジェクト作成

---
+++ situ_and_takyun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  situ_and_takyun
+//
+//  Created by vAlAi on 9/22/15.
+//  Copyright (c) 2015 vAlAi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ situ_and_takyunTests/situ_and_takyunTests.swift
@@ -0,0 +1,36 @@
+//
+//  situ_and_takyunTests.swift
+//  situ_and_takyunTests
+//
+//  Created by vAlAi on 9/22/15.
+//  Copyright (c) 2015 vAlAi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class situ_and_takyunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
